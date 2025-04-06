fbf72cdc6af606e0293d4c2f7c0a019945d296b3
TL2502 <chanunya2502.1112@gmail.com>
2025-04-07 01:14:00 +0700

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    compiler: {
-      styledComponents: true,
-    },
-  
-    images: {
-      unoptimized: true,  
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+
+  images: {
+    unoptimized: true,
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:shortUrlKey',  
+        destination: '/api/shorten/:shortUrlKey', 
+        permanent: true,  
+      },
+    ]
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/:shortUrlKey',
+        destination: '/api/shorten/:shortUrlKey', 
+      },
+    ]
+  },
+};
+
+export default nextConfig;
