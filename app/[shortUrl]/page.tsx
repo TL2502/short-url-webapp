3d8f7a618e6d4bc9d23373ff1a9ef70d25bc0561
TL2502 <chanunya2502.1112@gmail.com>
2025-04-07 17:00:26 +0700

    change back

--- app/[shortUrl]/page.tsx
+++ app/[shortUrl]/page.tsx
@@ -1,37 +1,20 @@
-import { GetServerSideProps } from 'next';
+import { redirect } from 'next/navigation';
 import prisma from "../../lib/db";
-import { NextResponse } from 'next/server';
 
 interface RedirectPageProps {
-  shortUrl: string;
-  originalUrl: string;
+  params: { shortUrl: string }; 
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const { shortUrl } = params as { shortUrl: string };
+export default async function RedirectPage({ params }: RedirectPageProps) {
+  const { shortUrl } = params;
 
   const url = await prisma.url.findUnique({
     where: { shortUrl: shortUrl },
   });
 
   if (!url) {
-    return {
-      notFound: true, 
-    };
+    return <div>404 - URL not found</div>;
   }
 
-  return {
-    props: {
-      shortUrl: shortUrl,
-      originalUrl: url.originalUrl,
-    },
-  };
-};
-
-export default function RedirectPage({ originalUrl }: RedirectPageProps) {
-  if (originalUrl) {
-    return NextResponse.redirect(originalUrl);
-  }
-
-  return <div>404 - URL not found</div>;
+  redirect(url.originalUrl); 
 }
